794a60e79599119ba904ec802d0b2b2ec429004e
andypagdin <andyjpagdin@gmail.com>
2017-03-08 22:48:42 +0000

    Convert XML file + Slider funcs
    Currently only handles one mood

--- app/moodslider/main.js
+++ app/moodslider/main.js
@@ -9,46 +9,61 @@ angular.module('myApp.moodslider', ['ngRoute'])
   });
 }])
 
-.controller('MoodsliderCtrl', ['$scope',function( $scope ) {
+.controller('MoodsliderCtrl', ['$scope', '$http', function( $scope, $http ) {
+
+	// Angular JS does not support the parsing of XML directly so we must use a lightweight
+	// library, such as x2js to return a JSON object from our XML file
+	$http.get("../content/programmes.xml", {
+		transformResponse: function (cnv) {
+		  var x2js = new X2JS();
+		  var aftCnv = x2js.xml_str2json(cnv);
+		  return aftCnv;
+		}
+	})
+
+	.success(function (response) {
+		$scope.programme = response
+		angular.forEach($scope.programme, function(item)
+		{
+			$scope.programme = item.programme
+			//console.log(item.programme)
+		});
+	});
 
 	$scope.slider = {
-		one: 50,
-		two: 50,
-		three: 50,
-		four: 50
+		agitated_calm: 1,
+		happy_sad: 1,
+		tired_wideawake: 1,
+		scared_fearless: 1
 	};
 
-	$scope.programmeList = [
-		{
-			programmeId : 1,
-			name : "Alice In Wonderland",
-			image : "../images/programmes/aliceinwonderland.jpg",
-			mood : "Calm"
-		},
-		{
-			programmeId : 2,
-			name : "Avatar",
-			image : "../images/programmes/avatar.jpg",
-			mood : "Fearless"	
-		},
-		{
-			programmeId : 3,
-			name : "Bruce Almighty",
-			image : "../images/programmes/brucealmighty.jpg",
-			mood : "Tired"
-		},
-		{
-			programmeId : 4,
-			name : "Goodfellas",
-			image : "../images/programmes/goodfellas.jpg",
-			mood : "Scared"
-		},
-		{
-			programmeId : 5,
-			name : "Interstellar",
-			image : "../images/programmes/interstellar.jpg",
-			mood : "Wide Awake"
+	//$scope.recommends = [];
+
+	$scope.moodCheck = function(moodVal, currentSlider){
+
+		if(moodVal > 1 && currentSlider == "ac"){
+			var currentMood = "Calm"
+		}else if(moodVal < 1 && currentSlider == "ac"){
+			var currentMood = "Agitated"
 		}
-	];
+
+		if(moodVal > 1 && currentSlider == "hc"){
+			var currentMood = "Sad"
+		}else if(moodVal < 1 && currentSlider == "hc"){
+			var currentMood = "Happy"
+		}
+
+		$scope.recommendedProgramme = $scope.programme.filter(function(i){
+			return i.mood == currentMood
+		});
+
+		// angular.forEach($scope.programme, function(item){
+		// 	if(item.mood == currentMood){
+		// 		$scope.newprogramme = item
+		// 		$scope.recommends.push(item)
+		// 		//console.log($scope.recommends)
+		// 	}
+		// })
+	}
 
 }]);
